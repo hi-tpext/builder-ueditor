8451c79689c752c3b9351ab3ea3f0c59a0b3133d
ichynul <ichynul@outlook.com>
2020-04-20 00:50:24 +0800

    update

--- src/common/Resource.php
+++ src/common/Resource.php
@@ -2,9 +2,9 @@
 
 namespace tpext\builder\ueditor\common;
 
-use tpext\common\Module as baseModule;
+use tpext\common\Resource as baseResource;
 
-class Module extends baseModule
+class Resource extends baseResource
 {
     protected $version = '1.0.1';
 
--- src/helper.php
+++ src/helper.php
@@ -3,7 +3,7 @@
 use tpext\common\ExtLoader;
 
 $classMap = [
-    'tpext\\builder\\ueditor\\common\\Module',
+    'tpext\\builder\\ueditor\\common\\Resource',
 ];
 
 ExtLoader::addClassMap($classMap);
